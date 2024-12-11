9c0ddd4a71838745ab7f7889ba958725a3d9c1ef
John Joseph <joseph.john33@yahoo.com>
2024-12-11 14:36:35 +0100

    tweaks

--- src/routes/Category/Sidebar/Rating.jsx
+++ src/routes/Category/Sidebar/Rating.jsx
@@ -1,19 +1,46 @@
+import { useState } from "react";
 import { getRatingStar } from "../../../db/Arrays";
 
+//Remember to commit and push
+
 const Rating = () => {
+  const [checked, setChecked] = useState([]);
+
+  //handlecheck
+  const handleCheck = (e) => {
+    const { name, checked } = e.target;
+    if (checked) {
+      setChecked((prev) => [...prev, name]);
+    } else {
+      setChecked((prev) => prev.filter((item) => item !== name));
+    }
+  };
+
+  console.log(checked);
+
   return (
     <div className="py-2 border-b">
       <h2 className="font-bold text-sm">Rating</h2>
       <div>
         <label className="pb-1 flex space-x-1 items-center">
-          <input type="checkbox" className="accent-green-600" />
+          <input
+            type="checkbox"
+            name="5.0"
+            onChange={handleCheck}
+            className="accent-green-600"
+          />
           <div className="flex space-x-1 text-[#ff8a00]">
             {getRatingStar(5)}
           </div>
           <span className="ml-2">5.0</span>
         </label>
         <label className="pb-1 flex space-x-1 items-center">
-          <input type="checkbox" className="accent-green-600" />
+          <input
+            type="checkbox"
+            name="4.0 & Up"
+            onChange={handleCheck}
+            className="accent-green-600"
+          />
           <div className="flex space-x-1 text-[#ff8a00]">
             {getRatingStar(4)}
           </div>
@@ -21,7 +48,12 @@ const Rating = () => {
           <span className="ml-2">4.0 & Up</span>
         </label>
         <label className="pb-1 flex space-x-1 items-center">
-          <input type="checkbox" className="accent-green-600" />
+          <input
+            type="checkbox"
+            name="3.0 & Up"
+            onChange={handleCheck}
+            className="accent-green-600"
+          />
           <div className="flex space-x-1 text-[#ff8a00]">
             {getRatingStar(3)}
           </div>
@@ -29,7 +61,12 @@ const Rating = () => {
           <span className="ml-2">3.0 & Up</span>
         </label>
         <label className="pb-1 flex space-x-1 items-center">
-          <input type="checkbox" className="accent-green-600" />
+          <input
+            type="checkbox"
+            name="2.0 & Up"
+            onChange={handleCheck}
+            className="accent-green-600"
+          />
           <div className="flex space-x-1 text-[#ff8a00]">
             {getRatingStar(3)}
           </div>
@@ -37,7 +74,12 @@ const Rating = () => {
           <span className="ml-2">2.0 & Up</span>
         </label>
         <label className="pb-1 flex space-x-1 items-center">
-          <input type="checkbox" className="accent-green-600" />
+          <input
+            type="checkbox"
+            name="1.0 & Up"
+            onChange={handleCheck}
+            className="accent-green-600"
+          />
           <div className="flex space-x-1 text-[#ff8a00]">
             {getRatingStar(1)}
           </div>
--- src/routes/Category/Sidebar/SideCategories.jsx
+++ src/routes/Category/Sidebar/SideCategories.jsx
@@ -1,35 +1,85 @@
+import { useState } from "react";
+
+//Remember to commit and push
+
 const SideCategories = () => {
+  const [selected, setSelected] = useState("");
+
+  //handlechange
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSelected(value);
+  };
+
+  //radio buttons
+  const { fruit, vegetables, grain, groceries, beverages, edibleOil } = {
+    fruit: "fruit",
+    vegetables: "vegetables",
+    grain: "grain",
+    groceries: "groceries",
+    beverages: "beverages",
+    edibleOil: "edibleOil",
+  };
+
+  console.log(selected);
+
   return (
-    <div className="border-b pb-1">
+    <form onChange={handleChange} className="border-b pb-1">
       <label className="pb-2 font-light text-sm flex ">
         <input
           type="radio"
           name="test"
+          value={fruit}
           className="mt-1 accent-green-600 mr-1"
         />
         Fresh Fruit
       </label>
       <label className="pb-2 font-light text-sm flex ">
-        <input type="radio" name="test" className="accent-green-600 mr-1" />
+        <input
+          type="radio"
+          name="test"
+          value={vegetables}
+          className="accent-green-600 mr-1"
+        />
         Vegetables
       </label>
       <label className="pb-2 font-light text-sm flex ">
-        <input type="radio" name="test" className="accent-green-600 mr-1" />
+        <input
+          type="radio"
+          name="test"
+          value={grain}
+          className="accent-green-600 mr-1"
+        />
         Grain
       </label>
       <label className="pb-2 font-light text-sm flex ">
-        <input type="radio" name="test" className="accent-green-600 mr-1" />
+        <input
+          type="radio"
+          name="test"
+          value={groceries}
+          className="accent-green-600 mr-1"
+        />
         Groceries
       </label>
       <label className="pb-2 font-light text-sm flex ">
-        <input type="radio" name="test" className="accent-green-600 mr-1" />
+        <input
+          type="radio"
+          name="test"
+          value={beverages}
+          className="accent-green-600 mr-1"
+        />
         Beverages
       </label>
       <label className="pb-2 font-light text-sm flex ">
-        <input type="radio" name="test" className="accent-green-600 mr-1" />
+        <input
+          type="radio"
+          name="test"
+          value={edibleOil}
+          className="accent-green-600 mr-1"
+        />
         Edible Oil
       </label>
-    </div>
+    </form>
   );
 };
 export default SideCategories;
